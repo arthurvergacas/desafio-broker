e80fa133a72cb8457c8dfde128edb67ca20876ce
Arthur Vergaças <arthur.martins@db.tec.br>
2023-01-30 19:52:42 -0300

    chore: add json serialization to dto

--- DesafioBroker/Brapi/Dtos/TickersQuotesList.cs
+++ DesafioBroker/Brapi/Dtos/TickersQuotesList.cs
@@ -1,8 +1,15 @@
+using Newtonsoft.Json;
+
 namespace DesafioBroker.Brapi.Dtos;
 
 public class TickersQuotesList
 {
     public IEnumerable<TickerQuotes> Results { get; set; } = null!;
+
     public DateTime RequestedAt { get; set; }
 
+    public override string ToString()
+    {
+        return JsonConvert.SerializeObject(this);
+    }
 }
