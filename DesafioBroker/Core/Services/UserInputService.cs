f7485229ddf4fd84626ae211bf2a7071536409ac
Arthur Vergaças <arthur.martins@db.tec.br>
2023-02-03 19:58:59 -0300

    feat: implement user input service

--- DesafioBroker/Core/Services/UserInputService.cs
+++ DesafioBroker/Core/Services/UserInputService.cs
@@ -1,5 +1,7 @@
 using DesafioBroker.Core.Interfaces;
+using DesafioBroker.Dtos;
 using DesafioBroker.StockSubscription.Dtos;
+using DesafioBroker.Misc;
 
 namespace DesafioBroker.Core.Services;
 
@@ -7,11 +9,49 @@ public class UserInputService : IUserInputService
 {
     public StockSubscriptionDto ParseUserInput(string[] args)
     {
-        throw new NotImplementedException();
+        if (args.Length != 3)
+        {
+            throw new ArgumentException(
+                "Arguments not passed correctly. The syntax is '<PROGRAM> <TICKER> <PURCHASE_REFERENCE> <SALE_REFERENCE>'"
+            );
+        }
+
+        var ticker = args[0];
+
+        decimal purchaseReferenceValue;
+        decimal saleReferenceValue;
+
+        try
+        {
+            purchaseReferenceValue = decimal.Parse(
+                args[1],
+                System.Globalization.NumberStyles.Float,
+                System.Globalization.CultureInfo.CurrentCulture
+            );
+            saleReferenceValue = decimal.Parse(
+                args[2],
+                System.Globalization.NumberStyles.Float,
+                System.Globalization.CultureInfo.CurrentCulture
+            );
+        }
+        catch
+        {
+            throw new ArgumentException("Unable to parse reference values to a number");
+        }
+
+        return new StockSubscriptionDto()
+        {
+            Ticker = ticker,
+            StockReferenceValues = new StockReferenceValuesDto(purchaseReferenceValue, saleReferenceValue)
+        };
     }
 
     public void WaitForUserCommands()
     {
-        throw new NotImplementedException();
+        var command = string.Empty;
+        while (command != Commands.EXIT)
+        {
+            command = Console.ReadKey().KeyChar.ToString();
+        }
     }
 }
