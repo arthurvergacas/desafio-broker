04f99bf4fcaeadfb0d37bfd0ad96202dc19737ed
Arthur Vergaças <arthur.martins@db.tec.br>
2023-02-03 23:39:38 -0300

    fix: make sure that the user isnt notified if stock quotes is null

--- DesafioBroker.Tests/StockSubscription/Services/StockSubscriptionServiceTest.cs
+++ DesafioBroker.Tests/StockSubscription/Services/StockSubscriptionServiceTest.cs
@@ -73,6 +73,22 @@ public class StockSubscriptionServiceTest
     }
 
     [Fact]
+    public void OnNotificationTimerEvent_WhenShouldNotifyUser_And_StockQuotesIsNull_ShouldNotSendMailNotification()
+    {
+        var service = this.CreateStockSubscriptionService();
+
+        this.SetupPurchaseScenario(service);
+
+        this.mockBrapiService
+            .Setup(service => service.GetStocksQuotesList(It.IsAny<IList<string>>()))
+            .ReturnsAsync(new StocksQuotesListDto());
+
+        service.OnNotificationTimerEvent(null, null!);
+
+        this.mockMailService.Verify(service => service.SendMail(It.IsAny<MailMessage>()), Times.Never());
+    }
+
+    [Fact]
     public async void GetSubscribedStockQuotes_ShouldCallBrapiApiWithSubscribedStockTicker()
     {
         var service = this.CreateStockSubscriptionService();
